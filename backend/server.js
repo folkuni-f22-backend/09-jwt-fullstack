5ca3c419d1ac9ee6d777bf3d0e4c33276d81b6a0
David Andersson <david.andersson@zocom.se>
2023-06-05 11:40:45 +0200

    Login route, create JWT

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 
 // konfigurera servern
 const app = express()
@@ -20,7 +21,48 @@ app.use((req, res, next) => {
 
 // routes
 // GET /public
+app.get('/public', (req, res) => {
+	res.send({
+		message: 'This is public data, anyone can see it.'
+	})
+})
+
 // POST /login
+app.post('/login', (req, res) => {
+	// Body: { username, password }
+	// Validate body
+	if( !req.body || !req.body.username || !req.body.password ) {
+		res.sendStatus(400)
+		return
+	}
+	const { username, password } = req.body
+
+	let found = users.find(user => user.username === username)
+	if( !found ) {
+		console.log('- felaktigt användarnamn')
+		res.sendStatus(401)
+		return
+	}
+	if( found.password !== password ) {
+		console.log('- felaktigt lösenord')
+		res.sendStatus(401)
+		return
+	}
+
+	// Lyckad inloggning! Skapa en JWT och skicka tillbaka
+	// jwt.sign(payload, secretOrPrivateKey, [options, callback])
+	const hour = 60 * 60
+	const payload = { userId: found.id }
+	const options = { expiresIn: 2 * hour }
+	let token = jwt.sign(payload, secret, options)
+	console.log('Signed JWT: ', token)
+	let tokenPackage = { token: token }
+	res.send(tokenPackage)
+})
+let users = [
+	{ id: 'a1', username: 'admin', password: 'password' }
+]
+
 // GET /secret
 
 // starta servern
