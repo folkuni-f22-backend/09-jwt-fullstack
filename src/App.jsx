28fcf3df1f314c8c6140c869c030afb7c3b0213d
David Andersson <david.andersson@zocom.se>
2023-06-05 14:37:03 +0200

    Frontend uses JWT to authenticate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+
+const sessionStorageKey = 'jwt-example'
 
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -7,8 +9,60 @@ function App() {
 	const [username, setUsername] = useState('')
 	const [password, setPassword] = useState('')
 
-	const handleLogin = async () => {}
-	const handleLogout = async () => {}
+	useEffect(() => {
+		if( sessionStorage.getItem(sessionStorageKey) ) {
+			setIsLoggedIn(true)
+		}
+	}, [])
+	
+	const handleLogin = async () => {
+		// förbereda request: body, options
+		// skicka request
+		// hantera svaret
+		let body = { username, password }
+		let options = {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(body)
+		}
+
+		const response = await fetch('/login', options)
+		if( response.status !== 200 ) {
+			setMessage('Det gick inte att logga in!')
+			console.log('Login failed with status: ', response.status)
+			return
+		}
+
+		const data = await response.json()
+		let jwt = data.token
+		sessionStorage.setItem(sessionStorageKey, jwt)
+
+		setIsLoggedIn(true)
+	}
+	const handleLogout = async () => {
+		sessionStorage.removeItem(sessionStorageKey)
+		setIsLoggedIn(false)
+	}
+
+	const handleGetData = async () => {
+		// fixa token - om den finns
+		// skicka request med fetch
+		// hantera svaret
+		let maybeJwt = sessionStorage.getItem(sessionStorageKey)
+		
+		let options = {
+			headers: {}
+		}
+		if( maybeJwt ) {
+			options.headers.Authorization = "Bearer: " + maybeJwt
+		}
+		
+		let response = await fetch('/secret', options)
+		let data = await response.json()
+		setMessage( data.message )
+	}
 
 	return (
 		<div>
@@ -39,7 +93,7 @@ function App() {
 
 			<div>
 				<h2> Hämta data från backend</h2>
-				<button> Hämta! </button>
+				<button onClick={handleGetData}> Hämta! </button>
 				<p> {message} </p>
 			</div>
 		</div>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+	proxy: {
+	  "/public": 'http://localhost:1337/',
+	  "/login": 'http://localhost:1337/',
+	  "/secret": 'http://localhost:1337/'
+	}
+  }
 })
