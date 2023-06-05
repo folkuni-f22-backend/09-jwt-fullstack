ea765e2018983cce53f7be9146c15abdf0281f9b
David Andersson <david.andersson@zocom.se>
2023-06-05 13:58:45 +0200

    Frontend form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const [message, setMessage] = useState('')
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleLogin = async () => {}
+	const handleLogout = async () => {}
+
+	return (
+		<div>
+			{isLoggedIn
+			? (
+				<div>
+					<h2> Logga ut</h2>
+					<button onClick={handleLogout}> Logga ut </button>
+				</div>
+			)
+			: (
+				<div>
+					<h2> Logga in </h2>
+					<input type="text" placeholder="user name"
+						onChange={e => setUsername(e.target.value)}
+						value={username}
+						/>
+					<input type="text" placeholder="password"
+						onChange={e => setPassword(e.target.value)}
+						value={password}
+						/>
+					<button type="button"
+						onClick={handleLogin}
+						> Logga in </button>
+				</div>
+			)
+			}
+
+			<div>
+				<h2> Hämta data från backend</h2>
+				<button> Hämta! </button>
+				<p> {message} </p>
+			</div>
+		</div>
+	)
 }
 
 export default App
